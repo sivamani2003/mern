d6c8d42dedf62b6b26a49d8b894103be563e7d41
siva <sivamani_k@srmap.edu.in>
2023-06-26 12:02:23 +0530

    update

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -1,19 +1,28 @@
-import {createTour} from './../Tour.js'
-export const createTour = async (req, res) => {
-    const newTour = new Tour(req.body);
-    
+import Tour from '../models/Tour.js'
+export const createTour = async(req,res)=>{
+    const newTour = new Tour(req.body)
+
     try {
-      const savedTour = await newTour.save();  // Save the new tour to the database
-      
-      res.status(200).json({
-        success: true,
-        message: 'Successfully created',
-        data: savedTour
-      });
+
+        const savedTour = await newTour.save()
+        res
+            .status(200)
+            .json({
+                sucess:true,
+                message:'Sucessfully created',
+                data:savedTour
+            })
+
     } catch (err) {
-      res.status(500).json({
-        success: false,
-        message: 'Failed to create. Try again'
-      });
+        res.status(500).json({sucess:false,message:'Failed to create.Try again'})
     }
-  };
+}
+
+
+export const updateTour = async(req,res)=>{
+    try {
+        
+    } catch (err) {
+        
+    }
+}
