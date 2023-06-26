d82d343945f27efdee02ec4e17e463035cd5bc11
siva <sivamani_k@srmap.edu.in>
2023-06-26 16:37:50 +0530

    login,register backend done

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import tourRoute from './routes/tours.js'
+import userRoute from './routes/users.js'
+import authRoute from './routes/auth.js'
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000
@@ -23,7 +25,9 @@ const connect= async()=>{
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
+app.use("/auth",authRoute);
 app.use("/tours",tourRoute);
+app.use("/users",userRoute);
 app.listen(port,()=>{
     connect();
     console.log('server listening on port',port)
